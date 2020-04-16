6d3c30741b032ea98ac88cc3bd1a60726a54f2b2
Razvp <razvanpetru@users.noreply.github.com>
2020-04-16 12:42:44 +0300

    adding checkbox for changing the background color

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,19 @@
 window.addEventListener("load", () => {
-  let long;
-  let lat;
+
   const currentLocation = document.querySelector("#location-timezone");
   const temperatureDegree = document.querySelector(".temperature");
   const temperatureSummary = document.querySelector("#temperature-description");
   const degree = document.querySelector('#deg');
   const loading = document.querySelector('.weather-loading');
+  const checkbox = document.getElementById('checkbox');
+
+  let long;
+  let lat;
 
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
+
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
@@ -21,7 +25,7 @@ window.addEventListener("load", () => {
           return response.json();
         })
         .then((data) => {
-          console.log(data);
+          // data from API
           const {
             temperature,
             summary,
@@ -31,18 +35,24 @@ window.addEventListener("load", () => {
           let celsius = (temperature - 32) * (5 / 9);
           // DOM elements
           loading.style.display = 'none';
+          // current location from API
           currentLocation.textContent = data.timezone;
+          // temperature from API in CELSIUS
           temperatureDegree.textContent = Math.floor(celsius);
+          // celsius symbol
           degree.innerHTML = '&#8451';
+          // summary from API
           temperatureSummary.textContent = summary;
-
+          // setting icons from SkyCons
           setIcons(icon, document.querySelector('#icon'));
         })
+
         .catch((error) => {
           console.log("Error", error);
         });
     });
 
+    // darkSky icons
     function setIcons(icon, iconID) {
       const skycons = new Skycons({
         color: 'white'
@@ -51,6 +61,20 @@ window.addEventListener("load", () => {
       skycons.play();
       return skycons.set(iconID, Skycons[currentIcon]);
     }
-
   }
 });
+
+// var for checkbox toggle
+const checkbox = document.getElementById('checkbox');
+const weatherContainer = document.getElementById('header__weather');
+const searchDesc = document.getElementById('description');
+const currentLocation = document.getElementById('current-location');
+
+checkbox.addEventListener('change', () => {
+  //body black color
+  document.body.classList.toggle('dark');
+
+  searchDesc.classList.toggle('dark');
+
+  currentLocation.classList.toggle('dark');
+})
--- server.js
+++ server.js
@@ -4,24 +4,16 @@ const path = require("path");
 
 const app = express();
 
+const PORT = 3000;
+
+
 app.use;
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
-app.use("/public", express.static(path.join(__dirname, "public")));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE" // what matters here is that OPTIONS is present
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
-const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
