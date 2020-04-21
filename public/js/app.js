debd9c8053a7c8971c517f5e945634e774193a19
Razvp <razvanpetru@users.noreply.github.com>
2020-04-21 23:49:56 +0300

    bug fixed

--- public/js/app.js
+++ public/js/app.js
@@ -65,3 +65,91 @@ window.addEventListener("load", () => {
     degree.innerHTML = "&#8451";
   }
 });
+
+const searchDesc = document.getElementById("description");
+const addLocation = document.getElementById("current-location");
+const weatherContainer = document.getElementById("header__weather");
+const dailyContainer = document.querySelectorAll(".daily__weather");
+const otherLocations = document.getElementById("other-locations");
+
+//search bar var
+const input = document.getElementById("search");
+const searchBar = document.getElementById("search-bar");
+const closeSearch = document.getElementById("close-search");
+closeSearch.style.display = 'none';
+
+// Btn for adding more locations
+function addLocations() {
+  let addLocations = document.getElementById("add-locations");
+  addLocations.addEventListener("click", () => {
+    weatherContainer.style.display = "none";
+    addLocation.innerHTML = "Add new location";
+    // add the search bar
+    searchBar.style.display = "block";
+    input.style.display = "block";
+    closeSearch.style.display = "block";
+
+    otherLocations.style.display = "none";
+
+    addLocations.style.display = "none";
+
+    searchDesc.style.display = "none";
+
+    closeSearch.addEventListener("click", () => {
+      window.history.go(0);
+    });
+  });
+}
+
+function weatherDetails() {
+  let addLocations = document.getElementById("add-locations");
+  weatherContainer.addEventListener("click", () => {
+    addLocations.style.display = "none";
+    otherLocations.style.display = "none";
+
+    dailyContainer.forEach((b) => {
+      b.style.display = "block";
+    });
+
+    searchDesc.style.display = "none";
+
+    closeSearch.style.display = "block";
+
+    closeSearch.addEventListener("click", () => {
+      window.history.go(0);
+    });
+  });
+}
+
+function theme() {
+  var themeSwitch = document.getElementById('checkbox');
+  if (themeSwitch) {
+    initTheme(); // on page load, if user has already selected a specific theme -> apply it
+
+    themeSwitch.addEventListener('change', function (event) {
+      resetTheme(); // update color theme
+    });
+
+    function initTheme() {
+      var darkThemeSelected = (localStorage.getItem('themeSwitch') !== null && localStorage.getItem('themeSwitch') === 'dark');
+      // update checkbox
+      themeSwitch.checked = darkThemeSelected;
+      // update body data-theme attribute
+      darkThemeSelected ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
+    };
+
+    function resetTheme() {
+      if (themeSwitch.checked) { // dark theme has been selected
+        document.body.setAttribute('data-theme', 'dark');
+        localStorage.setItem('themeSwitch', 'dark'); // save theme selection 
+      } else {
+        document.body.removeAttribute('data-theme');
+        localStorage.removeItem('themeSwitch'); // reset theme selection 
+      }
+    };
+  }
+}
+
+theme();
+addLocations();
+weatherDetails();
