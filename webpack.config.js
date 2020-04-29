2e913ab970cfb7856f9ae79f45de35721e8d1c26
Razvp <razvanpetru@users.noreply.github.com>
2020-04-29 17:14:02 +0300

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       filename: "index.html",
-      template: "./src/index.html",
+      template: "./index.html",
     }),
     new Dotenv(),
     new MomentLocalesPlugin(),
